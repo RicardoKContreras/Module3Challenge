9fe7b260d49a1388f55c5e8daaedbd2f1508dad8
Jimbei <contrerasr2272@gmail.com>
2022-05-22 23:54:20 -0400

    trying to submit work

--- Develop/script.js
+++ Develop/script.js
@@ -6,9 +6,41 @@ var generateBtn = document.querySelector("#generate");
 
 
 // Write password to the #password input
-function writePassword() {
- 
-    window.alert("We need to ask you a couple of questions to generate a password.");
+ function writePassword() {
+
+  var symbolOne = "!"
+  var symbolTwo = "@"
+  var symbolThree = "#"
+  
+  
+  var promptLength = window.prompt("How long would you like your password?");
+  
+    if (promptLength < 8 || promptLength > 128){
+      window.alert("Please choose a number between 8 and 128 characters");
+      writePassword();
+      
+
+  }
+  
+    else { 
+      window.prompt("Now, What character types would you like: " + symbolOne + "," + symbolTwo + "," + symbolThree);
+    }
+
+    var someFar = promptLength + symbolOne;
+    window.alert(someFar);
+
+      
+
+  
+     
+      
+
+    
+
+
+    
+
+  
   
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -16,10 +48,10 @@ function writePassword() {
   passwordText.value = password;
 
 }
+ 
+ writePassword();
 
 // Add event listener to generate button
+
+
 generateBtn.addEventListener("click", writePassword);
-
-
-
-var promptLength = window.prompt = "How long would you like your password to be?"
