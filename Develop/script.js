aa5e4fb61311732f3844c4f5f1ba0d2558c0a0a2
Jimbei <contrerasr2272@gmail.com>
2022-05-23 22:54:46 -0400

    working on symbols

--- Develop/script.js
+++ Develop/script.js
@@ -1,57 +1,115 @@
 // Assignment code here
-
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+// Get references to the #generate element
 
 // Write password to the #password input
  function writePassword() {
-
-  var symbolOne = "!"
-  var symbolTwo = "@"
-  var symbolThree = "#"
   
-  
-  var promptLength = window.prompt("How long would you like your password?");
-  
-    if (promptLength < 8 || promptLength > 128){
-      window.alert("Please choose a number between 8 and 128 characters");
-      writePassword();
-      
-
-  }
-  
-    else { 
-      window.prompt("Now, What character types would you like: " + symbolOne + "," + symbolTwo + "," + symbolThree);
-    }
-
-    var someFar = promptLength + symbolOne;
-    window.alert(someFar);
-
-      
-
-  
-     
-      
-
-    
-
-
-    
-
-  
-  
+ 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
-}
+ }
  
- writePassword();
+
+ 
+  var symbol = ["!", "@", "#"]
+  var lowerCase = "lowercase"
+  var upperCase = "uppercase"
+  var allCase = "Both"
+  var numericValue = Math.floor(Math.random() *10);
+  console.log(numericValue);
+  
+  
+  
+  function generatePassword(){
+  var promptLength = window.prompt("How long would you like your password?");
+  
+    if (promptLength < 8 || promptLength > 128){
+      window.alert("Please choose a number between 8 and 128 characters");
+      console.log(promptLength);
+      generatePassword();
+      
+
+  }
+  
+  
+
+    else if (promptLength >= 8 || promptLength <= 128){
+      window.alert("Next is style.");
+      
+    }
+    function Numbers() {
+    var numbers = confirm ("Do you want numbers?");
+
+    if (numbers === true){
+      console.log(numericValue);
+      
+    
+    }
+
+    
+      
+    }
+    Numbers();
+    function Styles(){
+    
+
+    
+      var passwordCase = window.prompt("Would you like uppercase, lowercase, or both?")
+    
+
+    if (passwordCase === "lowercase" || passwordCase === "LOWERCASE"){
+      console.log(lowerCase);
+    }
+
+    if(passwordCase === "uppercase" || passwordCase === "Uppercase"){
+      console.log(upperCase);
+    }
+
+    else if (passwordCase === "both" || passwordCase === "BOTH") {
+      console.log(allCase);
+    }
+
+    else {
+      window.alert("Please choose an answer");
+     
+    }
+    
+  }
+  Styles();
+    
+    //var passwordContent = [promptLength, numericValue];
+  //console.log(passwordContent);
+
+    
+
+    var someFar = promptLength + symbol[2];
+    window.alert(someFar);
+
+    
+  
+  
+}
+   
+
+    generateBtn.addEventListener("click", writePassword);
+
+    
+
+  writePassword();
+  generatePassword();
+ 
+    
+
+  
+  
+ 
+
+
 
 // Add event listener to generate button
 
 
-generateBtn.addEventListener("click", writePassword);
